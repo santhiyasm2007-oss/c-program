236d882ce8faed755517077d7d77e61a630d315e
santhiyasm2007-oss <santhiyasm2007@gmail.com>
2026-02-09 14:27:45 +0530

    Add files via upload

---
+++ main (12).c
@@ -0,0 +1,16 @@
+
+
+#include <stdio.h>
+ 
+int main()
+{
+
+    int a = 15;
+    int b = 70;
+    
+    printf("%d",a=a+5);
+    printf("\n%d",b=b+30);
+    
+
+    return 0;
+}
---
+++ main (8).c
@@ -0,0 +1,15 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+
+    int a = 10;
+    int b = 50;
+    printf("%d",a );
+    printf("\n%d",b);
+    
+
+    return 0;
+}
