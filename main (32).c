e66661778ef793087785076ff891136161a04a6c
santhiyasm2007-oss <santhiyasm2007@gmail.com>
2026-02-10 13:07:06 +0530

    Add files via upload

---
+++ main (32).c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+	int a,b,c;
+
+	scanf("%d %d %d",&a,&b, &c);
+	printf("%d", a+b+c);
+
+	return 0;
+}
