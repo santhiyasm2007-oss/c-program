10f5f6720023ebc8a556dfb8556b2a294efbe3ac
santhiyasm2007-oss <santhiyasm2007@gmail.com>
2026-02-11 11:18:14 +0530

    Add files via upload

---
+++ main (52).c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d %d", &a,&b);
+   
+   c=a;
+    printf("%d%d",a/=b,c%=b);
+
+
+}
