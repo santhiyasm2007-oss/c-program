bdfa35bc09f7b559c586950aa796017d8af2bb0b
santhiyasm2007-oss <santhiyasm2007@gmail.com>
2026-02-12 13:40:01 +0530

    Add files via upload

---
+++ main (68).c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+******************************************************************************/
+
+#include <stdio.h>
+
+int main() {
+    int a,b,c;
+    scanf("%d %d %d", &a,&b,&c);
+
+    if (a==b && b==c){
+        printf("Equilateral triangle");
+    }
+    else
+    printf("not Equilateral triangle");
+
+    return 0;
+}
