dbcd8162e052af322dd60cfb1f89b2dae37a67a3
santhiyasm2007-oss <santhiyasm2007@gmail.com>
2026-02-12 13:42:52 +0530

    Add files via upload

---
+++ main (69).c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>=10 && a<=20){
+        printf("number is with in range");
+        
+    }
+    else
+    printf("number is out side range");
+    
+    
+
+    return 0;
+}
