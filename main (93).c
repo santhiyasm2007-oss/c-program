f49a2de87b757a1deb6e69ce2382e711fd82c632
santhiyasm2007-oss <santhiyasm2007@gmail.com>
2026-02-14 16:02:47 +0530

    Add files via upload

---
+++ main (93).c
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int sum =0;
+    scanf("%d",&n);
+    for(int i=1;i<n;i++){
+       if(n%i==0){
+           sum+=i;
+       }
+    }
+    if(sum == n){
+        printf("Yes");
+        
+    }
+    else
+    printf("No");
+
+    return 0;
+}
