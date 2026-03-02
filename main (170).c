f7ae8ce0546839144eea7833abe2b08f8acaa2d2
santhiyasm2007-oss <santhiyasm2007@gmail.com>
2026-03-02 14:19:43 +0530

    Add files via upload

---
+++ main (170).c
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main() {
+    int i, j, n = 5;
+
+    for(i = 1; i <= n; i++) {
+        for(j = 1; j <= i; j++) printf("*");
+        for(j = 1; j <= 2*(n-i); j++) printf(" ");
+        for(j = 1; j <= i; j++) printf("*");
+        printf("\n");
+    }
+
+    for(i = n; i >= 1; i--) {
+        for(j = 1; j <= i; j++) printf("*");
+        for(j = 1; j <= 2*(n-i); j++) printf(" ");
+        for(j = 1; j <= i; j++) printf("*");
+        printf("\n");
+    }
+
+    return 0;
+}
