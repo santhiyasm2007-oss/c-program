6707f8e29a4586735dcb5273bff775bed25cb3a5
santhiyasm2007-oss <santhiyasm2007@gmail.com>
2026-03-13 11:20:05 +0530

    Add files via upload

---
+++ main (229).c
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i =0;
+    printf("Enter a string:");
+    fgets(str,sizeof(str),stdin);
+    while(str[i]!='\0'){
+        if(str[i]>'A'&&str[i]<='Z'){
+                str[i]=str[i]+32;
+            }
+            i++;
+
+    }
+    printf("%s",str);
+
+    return 0;
+}
